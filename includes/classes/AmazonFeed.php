c6ce1a8e6096f4d8b05d5cc397359234592be444
robbertdesmit <info@dutchdrops.com>
2016-02-01 10:06:35 +0100

    added external config possibility

--- includes/classes/AmazonFeed.php
+++ includes/classes/AmazonFeed.php
@@ -28,6 +28,7 @@ class AmazonFeed extends AmazonFeedsCore{
     private $response;
     private $feedContent;
     private $feedMD5;
+    public $storeData;
     
     /**
      * AmazonFeed submits a Feed to Amazon.
@@ -42,8 +43,8 @@ class AmazonFeed extends AmazonFeedsCore{
      * @param array|string $m [optional] <p>The files (or file) to use in Mock Mode.</p>
      * @param string $config [optional] <p>An alternate config file to set. Used for testing.</p>
      */
-    public function __construct($s = null, $mock = false, $m = null, $config = null){
-        parent::__construct($s, $mock, $m, $config);
+    public function __construct($s = null, $mock = false, $m = null, $config = null, $storeData = array()){
+        parent::__construct($s, $mock, $m, $config, $storeData);
         include($this->env);
         
         $this->options['Action'] = 'SubmitFeed';
